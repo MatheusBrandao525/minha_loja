d968595c316fe9c03bf82b12aeb3b427d6c3d010
MatheusBrandao525 <mafe123silva@gmail.com>
2024-03-30 20:51:54 -0400

    feat: :sparkles: Função para recuperar os dados do usuário logado na apliação.

--- models/UsuarioModel.php
+++ models/UsuarioModel.php
@@ -1,5 +1,21 @@
 <?php
-
 class UsuarioModel {
     
+    private $conexao;
+
+    public function __construct()
+    {
+        $this->conexao = Conexao::getInstance()->getConexao();
+    }
+
+    public function buscarDadosUsuarioLogado($idUsuarioLogado)
+    {
+        $sql = "SELECT * FROM usuarios WHERE id = :usuarioID";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindParam(':usuarioID', $idUsuarioLogado);
+
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }

ef4e89e269e914ff7a106b869b8f8aefb7ceb683
MatheusBrandao525 <mafe123silva@gmail.com>
2024-04-29 18:08:45 -0400

    feat: :memo: Adicionando imagens no banner.

--- components/banner.php
+++ components/banner.php
@@ -1,18 +1,32 @@
 <main class="banner">
     <div class="centro">
         <div class="banner-column">
-            <div class="small square" id="image1">1</div>
-            <div class="small square" id="image4">6</div>
+            <div class="small square" id="image1">
+                <img src="public/assets/img/site/banner_amortecedores.png" alt="Banner de amortecedores">
+            </div>
+            <div class="small square" id="image6">
+                <img src="public/assets/img/site/banner_retrovisores.png" alt="Banner de retrovisores">
+            </div>
         </div>
-        <div class="large rectangle" id="image2">2</div>
+        <div class="large rectangle" id="image2">
+            <img src="public/assets/img/site/banner_meio.png" alt="Banner de logo marca">
+        </div>
         <div class="banner-column column-maior">
             <div class="small square bg-white" id="image3">
                 <div class="rowBanner">
-                    <div class="coluna">3</div>
-                    <div class="coluna">4</div>
+                    <div class="coluna" id="image3">
+                        <img src="public/assets/img/site/banner_capacetes.png" alt="Banner de Capacetes">
+                    </div>
+
+                    <div class="coluna" id="image4">
+                        <img src="public/assets/img/site/banner_capa.png" alt="Banner de capa de chuva">
+                    </div>
                 </div>
             </div>
-            <div class="small square" id="image5">5</div>
+            <div class="small square" id="image5">
+                <img src="public/assets/img/site/banner_pneus.png" alt="Banner de Pneus">
+            </div>
+
         </div>
     </div>
 </main>

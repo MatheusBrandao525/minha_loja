97959c4f0b192d347178143c8e89b17607ad15a5
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-17 17:03:49 -0400

    style: :art: Responsividade banner 2 em telas de 768px.

--- components/banner-02.php
+++ components/banner-02.php
@@ -3,7 +3,6 @@
         <div class="slide" id="slide1"></div>
         <div class="slide" id="slide2">Slide 2</div>
         <div class="slide" id="slide3">Slide 3</div>
-        <!-- Adicione mais slides conforme necessário -->
     </div>
     <a class="prev" onclick="moveSlide(-1)">❮</a>
     <a class="next" onclick="moveSlide(1)">❯</a>
@@ -25,20 +24,17 @@ function moveSlide(step) {
         slideIndex = slides.length - 1;
     }
 
-    const offset = slideIndex * -100; // Assume que a largura de cada slide é 100% do container
+    const offset = slideIndex * -100;
     const slidesContainer = document.querySelector('.carousel .slides');
     slidesContainer.style.transform = `translateX(${offset}%)`;
 }
 
-// Função para mover o carrossel automaticamente
 function autoMoveSlide() {
     moveSlide(1);
 }
 
-// Configura o carrossel para se mover a cada 3 segundos
 setInterval(autoMoveSlide, 3000);
 
-// Adiciona suporte para gestos de toque
 const slidesContainer = document.querySelector('.carousel .slides');
 
 slidesContainer.addEventListener('touchstart', e => {
@@ -59,7 +55,6 @@ function handleGesture() {
     }
 }
 
-// Inicializa o carrossel mostrando o primeiro slide
 moveSlide(0);
 
 </script>

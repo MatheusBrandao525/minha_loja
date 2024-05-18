71ae1c12cf7ad2e9f398b78959fd7eeb8275b7ee
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-18 08:14:53 -0400

    style: :art: Responsividade banner em telas de 768px.

--- components/banner.php
+++ components/banner.php
@@ -12,13 +12,13 @@
             <img src="public/assets/img/site/banner_meio.png" alt="Banner de logo marca">
         </div>
         <div class="banner-column column-maior">
-            <div class="small square bg-white" id="image3">
+            <div class="small square bg-white square-responsivo" id="image3">
                 <div class="rowBanner">
-                    <div class="coluna" id="image3">
+                    <div class="coluna coluna-responsiva-image" id="image3">
                         <img src="public/assets/img/site/banner_capacetes.png" alt="Banner de Capacetes">
                     </div>
 
-                    <div class="coluna" id="image4">
+                    <div class="coluna coluna-responsiva-image" id="image4">
                         <img src="public/assets/img/site/banner_capa.png" alt="Banner de capa de chuva">
                     </div>
                 </div>

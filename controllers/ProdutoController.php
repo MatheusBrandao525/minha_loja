c5e0472e19e571ac1088a498c6add6edf2431775
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-21 13:10:10 -0400

    feat: :sparkles: Funções para exibir produtos por categoria, pesquisa e exibir todos os produtos.

--- controllers/ProdutoController.php
+++ controllers/ProdutoController.php
@@ -18,4 +18,36 @@ class ProdutoController
         $produtoModel = new ProdutoModel();
         return $produtoModel->buscarProdutosEmDestaque();
     }
+
+    public function exibirTodosOsProdutos()
+    {
+        $produtoModel = new ProdutoModel();
+        return $produtoModel->buscarTodosOsProdutos();
+    }
+
+    public function exibirProdutosPorCategoria()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['categoriaId'])) {
+            $categoriaId = intval($_POST['categoriaId']);
+
+            $produtoModel = new ProdutoModel();
+            return $produtoModel->buscarProdutosPorCategoria($categoriaId);
+        }else{
+            include ROOT_PATH . '/views/erro404.php';
+            die;
+        }
+    }
+
+    public function exibirProdutosPesquisados()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pesquisa'])) {
+            $pesquisa = filter_input(INPUT_POST, 'pesquisa', FILTER_SANITIZE_SPECIAL_CHARS);
+            $produtoModel = new ProdutoModel();
+
+            return $produtoModel->buscarProdutosPorPesquisa($pesquisa);
+        }else{
+            include ROOT_PATH . '/views/erro404.php';
+            die;
+        }
+    }
 }

e71b378f33256d5ef6872f30f9c9cfce65d0e708
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-26 09:54:20 -0400

    feat: :sparkles: Seção para exibir produtos em destaque.

--- components/produtos_destaque.php
+++ components/produtos_destaque.php
@@ -1,20 +1,20 @@
 <?php
-    require_once 'models/ProdutoModel.php';
-    $produtoModel = new ProdutoModel();
-    $produtosEmDestaque = $produtoModel->buscarProdutosEmDestaque();
+require_once 'models/ProdutoModel.php';
+$produtoModel = new ProdutoModel();
+$produtosEmDestaque = $produtoModel->buscarProdutosEmDestaque();
 ?>
-        <section class="special-offer">
-            <div class="container">
-                <h2>Especial Lingerie</h2>
-                <div class="offer-products">
-                    <div class="product">
-                        <img src="product1.png" alt="Produto 1">
-                        <p>A partir de R$ 9,99</p>
-                    </div>
-                    <div class="product">
-                        <img src="product2.png" alt="Produto 2">
-                        <p>A partir de R$ 9,99</p>
-                    </div>
-                </div>
+<section class="special-offer">
+    <div class="container">
+        <h2>Especial Lingerie</h2>
+        <div class="offer-products">
+            <div class="product">
+                <img src="product1.png" alt="Produto 1">
+                <p>A partir de R$ 9,99</p>
             </div>
-        </section>
+            <div class="product">
+                <img src="product2.png" alt="Produto 2">
+                <p>A partir de R$ 9,99</p>
+            </div>
+        </div>
+    </div>
+</section>

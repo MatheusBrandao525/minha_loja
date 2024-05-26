24e322de7b5794f0dc76e13fc36a04d066db11c4
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-26 09:52:58 -0400

    feat: :sparkles: Componente de categorias.

---
+++ components/categoriasSection.php
@@ -0,0 +1,45 @@
+<section class="categorias-circulos">
+    <h3>Navegue por categoria</h3>
+    <div class="categories-scroll">
+        <div class="category-item">
+            <img src="public/assets/img/placeholder.jpg" alt="Hardware">
+            <span class="category-label">Categoria1</span>
+        </div>
+        <div class="category-item">
+            <img src="public/assets/img/placeholder.jpg" alt="Periféricos">
+            <span class="category-label">Categoria2</span>
+        </div>
+        <div class="category-item">
+            <img src="public/assets/img/placeholder.jpg" alt="Smartphones">
+            <span class="category-label">Categoria3</span>
+        </div>
+        <div class="category-item">
+            <img src="public/assets/img/placeholder.jpg" alt="Smartphones">
+            <span class="category-label">Categoria4</span>
+        </div>
+        <div class="category-item">
+            <img src="public/assets/img/placeholder.jpg" alt="Smartphones">
+            <span class="category-label">Categoria5</span>
+        </div>
+        <div class="category-item">
+            <img src="public/assets/img/placeholder.jpg" alt="Smartphones">
+            <span class="category-label">Categoria6</span>
+        </div>
+        <div class="category-item">
+            <img src="public/assets/img/placeholder.jpg" alt="Smartphones">
+            <span class="category-label">Categoria7</span>
+        </div>
+        <div class="category-item">
+            <img src="public/assets/img/placeholder.jpg" alt="Smartphones">
+            <span class="category-label">Categoria8</span>
+        </div>
+        <div class="category-item">
+            <img src="public/assets/img/placeholder.jpg" alt="Smartphones">
+            <span class="category-label">Categoria9</span>
+        </div>
+        <div class="category-item">
+            <img src="public/assets/img/placeholder.jpg" alt="Smartphones">
+            <span class="category-label">Categoria10</span>
+        </div>
+    </div>
+</section>

05443d98b05a14f955738f4ac09cb3d63be8d4ad
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-26 09:53:47 -0400

    feat: :sparkles: Importando Componente de categorias.

--- views/home.php
+++ views/home.php
@@ -2,6 +2,7 @@
 include 'components/header.php';
 include 'components/banner.php';
 include 'components/banner-03.php';
+include 'components/categoriasSection.php';
 include 'components/produtos_destaque.php';
 // include 'components/banner-02.php';
 include 'components/footer.php';

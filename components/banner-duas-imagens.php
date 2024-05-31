ffb4bf466e3e5413b1eac3479e5e44bda9be0aa9
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-30 22:06:26 -0300

    feat: :sparkles: Componente de banner com duas imagens alinhadas na horizontal.

---
+++ components/banner-duas-imagens.php
@@ -0,0 +1,27 @@
+<style>
+    .centro {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .container-duas-imagens {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .imagem-banner-duas-imagens {
+        margin: 20px 10px;
+    }
+</style>
+<div class="centro">
+    <div class="container-duas-imagens">
+        <div class="imagem-banner-duas-imagens">
+            <img src="public/assets/img/site/imagem-banner-duas-imagens1.jpeg" alt="">
+        </div>
+        <div class="imagem-banner-duas-imagens">
+            <img src="public/assets/img/site/imagem-banner-duas-imagens2.jpeg" alt="">
+        </div>
+    </div>
+</div>

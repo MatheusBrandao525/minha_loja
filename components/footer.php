4c3d7f773f79e87b9d58907525c9b7445d2efa83
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-30 22:05:02 -0300

    feat: :sparkles: Difinido layout do footer.

--- components/footer.php
+++ components/footer.php
@@ -1,7 +1,219 @@
 </main>
 
 <footer>
-    <div class="container">
-        <p>&copy; 2024 Loja. Todos os direitos reservados.</p>
-    </div>
+    <section class="newsletter" data-position="2">
+        <div class="container">
+            <div class="newsletter-content">
+                <div class="social-networking-newsletter">
+                    <div class="row">
+                        <div class="social-title">Aproveite e compartilhe a Colt Bella nas Redes Sociais</div>
+                    </div>
+                    <div class="row">
+                        <div class="text-right social-icons">
+                            <a href="" target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
+                            <a href="" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+                            <a href="" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+                            <a href="" target="_blank"><i class="fab fa-youtube fa-2x"></i></a>
+                            <a href="" target="_blank"><i class="fab fa-pinterest fa-2x"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="yCmsContentSlot pre-footer">
+        <div class="hidden"></div>
+        <div class="container-footer">
+            <div class="container-fluid">
+                <div class="footer__top">
+                    <div class="row-footer">
+                        <div class="footer__right" data-position="2">
+                            <p class="search-title c-03">Não encontrou o que estava procurando?</p>
+
+                            <div class="search-content">
+                                <p>
+                                    <a href="/faq">
+                                        <img src="https://images2.marisa.com.br/_ui/responsive/theme-marisa/img/ico_ajuda.png" alt="icone de ajuda">
+                                        Podemos ajudar? Fale conosco.
+                                    </a>
+                                </p>
+                                <p>
+                                    <a href="/store-finder">
+                                        <img src="https://images2.marisa.com.br/_ui/responsive/theme-marisa/img/ico_localizacao_cinza.png" alt="icone de localizacao">
+                                        Encontre as lojas mais próximas.
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="footer__left">
+                            <div class="row-footer-left">
+                                <div class="footer__nav--container ">
+                                    <div class="title">Departamentos</div>
+                                    <ul class="footer__nav--links" id="collapseDepartamentosNavNode">
+                                        <li class="footer__link ">
+                                            <a href="" title="Feminino"> Feminino</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Calçados"> Calçados</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Lingerie"> Lingerie</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Jeans"> Jeans</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Plus Size"> Plus Size</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Acessórios"> Acessórios</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Infantil"> Infantil</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Masculino"> Masculino</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Marcas"> Marcas</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Roupas Femininas"> Roupas Femininas</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Blusas"> Blusas</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="footer__nav--container">
+                                    <div class="title">Atendimento ao Cliente</div>
+                                    <ul class="footer__nav--links" id="collapseSuporteNavNode">
+                                        <li class="footer__link ">
+                                            <a href="" title="Nossos canais de Atendimento"> Nossos canais de Atendimento</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Fale com a gente"> Fale com a gente</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Formas de Pagamento"> Formas de Pagamento</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Troca e Devoluções"> Troca e Devoluções</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Meus Pedidos"> Meus Pedidos</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Como Comprar"> Como Comprar</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Prazo de Entrega"> Prazo de Entrega</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="2ª Via de Boleto"> 2ª Via de Boleto</a>
+                                        </li>
+                                        <li class="footer__link ">
+                                            <a href="" title="Regras e Promoções"> Regras e Promoções</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="yCmsContentSlot bottom-footer">
+        <style type="text/css">
+            .footer .bottom-footer {
+                padding-bottom: 2em;
+            }
+        </style>
+        <div class="selosSegurancaFooter p-b-20 hidden-sm hidden-xs">
+            <div class="container">
+                <div class="d-flex v-center">
+                    <div class="imgSegurancaFooter m-r-40">
+                        <div class="d-flex v-center">
+                            <div class="js-container-preload container-preload"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/h46/hf2/9762966667294/ico-seguranca.png" class="m-w-100 h-auto" alt="Selo de Segurança" width="31" height="31"></div>
+                            <p class="f-f-josefin fs-16 f-w-bold m-0 p-l-20">Selos de segurança</p>
+                        </div>
+                    </div>
+                    <div class="allSelosFooter">
+                        <div class="d-flex v-center">
+                            <div class="js-container-preload container-preload p-r-12"><a id="seloEbit" href="http://www.ebit.com.br/898/selo" target="_blank" data-noop="redir(this.href);" title="Bronze" style="display: block; overflow: hidden; position: relative;"><img src="https://newimgebit-a.akamaihd.net/ebitBR/selo/img_898.png?1717114471820" alt="Bronze" style="border: 0px;"></a>
+                                <script type="text/javascript" id="getSelo" src="https://imgs.ebit.com.br/ebitBR/selo-ebit/js/getSelo.js?898"> </script>
+                            </div>
+                            <div class="js-container-preload container-preload p-r-12"><a href="https://br.clear.sale/lp/antifraude-ecommerce?ads_cmpid=793145905&amp;ads_adid=42883462713&amp;ads_matchtype=e&amp;ads_network=g&amp;ads_content=189988131793&amp;utm_term=clearsale&amp;ads_targetid=kwd-3762224881&amp;utm_campaign=&amp;utm_source=adwords&amp;utm_medium=ppc&amp;gclid=EAIaIQobChMIv6Xd25zW2wIVBwiRCh272AdPEAAYASAAEgLHq_D_BwE" target="_blank"><img data-src="https://images2.marisa.com.br/medias/sys_master/images/images/h60/h18/10252886835230/clearsale.png" class="m-w-100 h-auto lazy" alt="clearsale" width="122" height="30" src="https://images2.marisa.com.br/medias/sys_master/images/images/h60/h18/10252886835230/clearsale.png"></a></div>
+                            <div class="js-container-preload container-preload p-r-12"><a href="http://www.camara-e.net/" target="_blank"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/hc2/hbf/9762957426718/camerae-badge.png" class="m-w-100 h-auto" alt="Camara e-net" width="85" height="15"> </a></div>
+                            <div class="js-container-preload container-preload p-r-12"><a href="https://www.verisign.com.br/" target="_blank"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/hac/h14/9762959982622/verisign-badge.png" class="m-w-100 h-auto" alt="Verisign" width="90" height="40"> </a></div>
+                            <div class="js-container-preload container-preload p-r-12"><a href="http://www.internetsegura.org/" target="_blank"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/h48/h8e/9762960900126/internetsegura-badge.png" class="m-w-100 h-auto" alt="Internet Segura" width="45" height="23"></a></div>
+                            <div class="js-container-preload container-preload p-r-12"><a id="seloEconfy" href="https://confi.com.vc/lojas-confiaveis/detalhes?id=110824" target="_blank" datanoop="redir(this.href);"><img data-src="https://cdn.confi.com.vc/reputation/110824.png" class="lazy" src="https://cdn.confi.com.vc/reputation/110824.png"></a>
+                                <script type="text/javascript" id="getData" src="https://selo.compreconfie.com.br/scripts/getData.js?sellerId=110824"></script>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hidden" data-component="CMSParagraphComponentModel (8796123464764@11)"></div>
+        <p>
+            <style type="text/css">
+                .formasPagamentoFooter p {
+                    color: #5d5d5d
+                }
+
+                .formasPagamentoFooter .imgPgtoFooter {
+                    width: 20%
+                }
+
+                .formasPagamentoFooter .allPagamentos {
+                    width: 80%
+                }
+            </style>
+        </p>
+        <div class="formasPagamentoFooter hidden-sm hidden-xs m-b-20">
+            <div class="container">
+                <div class="d-flex v-center">
+                    <div class="imgPgtoFooter">
+                        <div class="d-flex v-center">
+                            <div class="js-container-preload container-preload"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/hef/hd3/9762965454878/ico-cartaocredito-footer.png" class="m-w-100 h-auto" alt="Formas de pagamento" width="31" height="31"></div>
+                            <p class="f-f-josefin fs-16 f-w-bold m-0 p-l-20">Formas de pagamento</p>
+                        </div>
+                    </div>
+                    <div class="allPagamentos">
+                        <div class="d-flex v-center">
+                            <div class="js-container-preload container-preload p-r-12"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/hcc/he4/11130261798942/novocartaomarisa-icon.jpg" class="m-w-100 h-auto" alt="Cartão Marisa" width="48" height="30"></div>
+                            <div class="js-container-preload container-preload p-r-12"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/h01/h05/11130261733406/novocartaomarisaitau-icon.jpg" class="m-w-100 h-auto" alt="Cartão Marisa Itaú" width="48" height="30"></div>
+                            <div class="js-container-preload container-preload p-r-12"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/h77/h00/9762946908190/amex-icon.png" class="m-w-100 h-auto" alt="Amex" width="48" height="30"></div>
+                            <div class="js-container-preload container-preload p-r-12"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/hfa/h04/9762947563550/hipercard-icon.png" class="m-w-100 h-auto" alt="hipercard" width="48" height="30"></div>
+                            <div class="js-container-preload container-preload p-r-12"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/h22/hcc/9762948612126/diners-icon.png" class="m-w-100 h-auto" alt="Dinners" width="48" height="30"></div>
+                            <div class="js-container-preload container-preload p-r-12"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/h6b/hce/9762949955614/mastercard-icon.png" class="m-w-100 h-auto" alt="Mastercard" width="48" height="30"></div>
+                            <div class="js-container-preload container-preload p-r-12"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/h9b/h15/9762951233566/visa-icon.png" class="m-w-100 h-auto" alt="Visa" width="48" height="30"></div>
+                            <div class="js-container-preload container-preload p-r-12"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/h3d/hb8/9762951659550/elo-icon.png" class="m-w-100 h-auto" alt="Elo" width="48" height="30"></div>
+                            <div class="js-container-preload container-preload p-r-12"><img src="https://images2.marisa.com.br/medias/sys_master/images/images/h15/h31/9762952151070/boleto-icon.png" class="m-w-100 h-auto" alt="Boleto" width="48" height="30"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hidden" data-component="CMSParagraphComponentModel (9025371767868@6)"></div>
+        <p>
+            <style type="text/css">
+                #copy-text {
+                    display: none;
+                    visibility: hidden;
+                }
+
+                #text-to-copy {
+                    display: none;
+                    visibility: hidden;
+                }
+            </style>
+        </p>
+    </section>
 </footer>

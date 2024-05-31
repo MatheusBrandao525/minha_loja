a1a2127746c0c7278fd15febe088aea0c02ba2d0
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-30 22:06:47 -0300

    feat: :sparkles: Componentes de banner com imagem grande.

---
+++ components/banner-imagem-grande2.php
@@ -0,0 +1,16 @@
+<style>
+    .centro {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+<div class="centro">
+    <div class="container-imagem-grande">
+        <a href="">
+            <img src="public/assets/img/site/imagem-banner-grande-dois.jpeg" alt="">
+        </a>
+    </div>
+
+</div>

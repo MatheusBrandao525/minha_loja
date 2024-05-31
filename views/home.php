1ceb8c84a6d011df2cd6bec3f61e877477869053
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-30 22:05:22 -0300

    feat: :sparkles: Inclusão de novos componentes.

--- views/home.php
+++ views/home.php
@@ -3,7 +3,11 @@ include 'components/header.php';
 include 'components/banner.php';
 include 'components/banner-03.php';
 include 'components/categoriasSection.php';
+include 'components/banner-imagem-grande.php';
+include 'components/navegue-por-preco.php';
+include 'components/banner-duas-imagens.php';
+include 'components/banner-imagem-grande2.php';
 include 'components/produtos_destaque.php';
+include 'components/produtos_novidades.php';
 // include 'components/banner-02.php';
 include 'components/footer.php';
-?>

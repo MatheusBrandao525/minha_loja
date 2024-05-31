a6cb814400b5eb4a8c8dc5d311591a671f55e14d
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-30 22:08:04 -0300

    feat: :sparkles: Componente para exibição de produtos novidades com layout definido.

---
+++ components/produtos_novidades.php
@@ -0,0 +1,104 @@
+<?php
+require_once 'models/ProdutoModel.php';
+$produtoModel = new ProdutoModel();
+$produtosEmDestaque = $produtoModel->buscarProdutosEmDestaque();
+?>
+<section class="special-offer">
+    <div class="container-produto-destaque">
+        <h2>novidades</h2>
+        <div class="centro">
+            <div class="offer-products">
+                <div class="product">
+                    <img src="public/assets/img/produto-exemplo.jpeg" alt="Produto 1">
+                    <p class="product-name">Produto para efetuar testes. Produto para efetuar testes.</p>
+                    <div class="precos">
+                        <div class="old-price">De 99,99</div>
+                        <div class="price">Por: 79,99</div>
+                        <div class="installments">5x de 16,00 sem juros</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="public/assets/img/produto-exemplo.jpeg" alt="Produto 2">
+                    <p class="product-name">Produto para efetuar testes. Produto para efetuar testes.</p>
+                    <div class="precos">
+                        <div class="old-price">De 99,99</div>
+                        <div class="price">Por: 79,99</div>
+                        <div class="installments">5x de 16,00 sem juros</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="public/assets/img/produto-exemplo.jpeg" alt="Produto 2">
+                    <p class="product-name">Produto para efetuar testes. Produto para efetuar testes.</p>
+                    <div class="precos">
+                        <div class="old-price">De 99,99</div>
+                        <div class="price">Por: 79,99</div>
+                        <div class="installments">5x de 16,00 sem juros</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="public/assets/img/produto-exemplo.jpeg" alt="Produto 2">
+                    <p class="product-name">Produto para efetuar testes. Produto para efetuar testes.</p>
+                    <div class="precos">
+                        <div class="old-price">De 99,99</div>
+                        <div class="price">Por: 79,99</div>
+                        <div class="installments">5x de 16,00 sem juros</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="public/assets/img/produto-exemplo.jpeg" alt="Produto 2">
+                    <p class="product-name">Produto para efetuar testes. Produto para efetuar testes.</p>
+                    <div class="precos">
+                        <div class="old-price">De 99,99</div>
+                        <div class="price">Por: 79,99</div>
+                        <div class="installments">5x de 16,00 sem juros</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="public/assets/img/produto-exemplo.jpeg" alt="Produto 2">
+                    <p class="product-name">Produto para efetuar testes. Produto para efetuar testes.</p>
+                    <div class="precos">
+                        <div class="old-price">De 99,99</div>
+                        <div class="price">Por: 79,99</div>
+                        <div class="installments">5x de 16,00 sem juros</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="public/assets/img/produto-exemplo.jpeg" alt="Produto 2">
+                    <p class="product-name">Produto para efetuar testes. Produto para efetuar testes.</p>
+                    <div class="precos">
+                        <div class="old-price">De 99,99</div>
+                        <div class="price">Por: 79,99</div>
+                        <div class="installments">5x de 16,00 sem juros</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="public/assets/img/produto-exemplo.jpeg" alt="Produto 2">
+                    <p class="product-name">Produto para efetuar testes. Produto para efetuar testes.</p>
+                    <div class="precos">
+                        <div class="old-price">De 99,99</div>
+                        <div class="price">Por: 79,99</div>
+                        <div class="installments">5x de 16,00 sem juros</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="public/assets/img/produto-exemplo.jpeg" alt="Produto 2">
+                    <p class="product-name">Produto para efetuar testes. Produto para efetuar testes.</p>
+                    <div class="precos">
+                        <div class="old-price">De 99,99</div>
+                        <div class="price">Por: 79,99</div>
+                        <div class="installments">5x de 16,00 sem juros</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="public/assets/img/produto-exemplo.jpeg" alt="Produto 2">
+                    <p class="product-name">Produto para efetuar testes. Produto para efetuar testes.</p>
+                    <div class="precos">
+                        <div class="old-price">De 99,99</div>
+                        <div class="price">Por: 79,99</div>
+                        <div class="installments">5x de 16,00 sem juros</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

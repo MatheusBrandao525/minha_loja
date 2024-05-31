f44dc1e1d598dc9934b5870023ba34a8a7b3e3f6
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-30 22:07:17 -0300

    feat: :sparkles: Componente para navegação por faixa de preços.

---
+++ components/navegue-por-preco.php
@@ -0,0 +1,58 @@
+<style>
+    .centro {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .precos-container {
+        padding: 20px 0;
+        width: 95%;
+    }
+
+    .titulo-precos-container {
+        text-align: center;
+        font-size: 1.5rem;
+        font-weight: bold;
+        color: #717171;
+    }
+
+    .precos-container ul {
+        margin: 20px 0;
+        display: flex;
+        width: 100% !important;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .precos-container ul li {
+        width: 80px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
+        color: #ec008c;
+        background-color: #f1f1f1;
+        text-align: center;
+        border-radius: 10px;
+    }
+</style>
+
+<div class="centro">
+    <div class="precos-container">
+        <h4 class="titulo-precos-container">Navegue por faixa de preço</h4>
+        <ul>
+            <li>R$ 00,00</li>
+            <li>R$ 00,00</li>
+            <li>R$ 00,00</li>
+            <li>R$ 00,00</li>
+            <li>R$ 00,00</li>
+            <li>R$ 00,00</li>
+            <li>R$ 00,00</li>
+            <li>R$ 00,00</li>
+        </ul>
+    </div>
+</div>

4fc1de983a39cf207abf7cae7eeb33407eeec400
MatheusBrandao525 <mafe123silva@gmail.com>
2025-01-14 17:49:30 -0400

    feat: :sparkles: Alteração nos banners do site.

--- components/banner.php
+++ components/banner.php
@@ -1,30 +1,31 @@
 <main class="banner">
     <div class="centro">
-        <div class="banner-column">
-            <div class="small square" id="image1">
-                <img src="public/assets/img/site/banner_amortecedores.png" alt="Banner de amortecedores">
-            </div>
-            <div class="small square" id="image6">
-                <img src="public/assets/img/site/banner_retrovisores.png" alt="Banner de retrovisores">
-            </div>
-        </div>
+
         <div class="large rectangle" id="image2">
             <img src="public/assets/img/site/banner_meio.png" alt="Banner de logo marca">
+        </div>
+        <div class="banner-column">
+            <div class="small square" id="image1">
+                <img src="public/assets/img/site/banner_brinquedos.png" alt="Banner de amortecedores">
+            </div>
+            <div class="small square" id="image6">
+                <img src="public/assets/img/site/banner_luminarias.png" alt="Banner de retrovisores">
+            </div>
         </div>
         <div class="banner-column column-maior">
             <div class="small square bg-white square-responsivo" id="image3">
                 <div class="rowBanner">
                     <div class="coluna coluna-responsiva-image" id="image3">
-                        <img src="public/assets/img/site/banner_capacetes.png" alt="Banner de Capacetes">
+                        <img src="public/assets/img/site/banner_vasos.png" alt="Banner de Capacetes">
                     </div>
 
                     <div class="coluna coluna-responsiva-image" id="image4">
-                        <img src="public/assets/img/site/banner_capa.png" alt="Banner de capa de chuva">
+                        <img src="public/assets/img/site/banner_quadros.png" alt="Banner de capa de chuva">
                     </div>
                 </div>
             </div>
             <div class="small square" id="image5">
-                <img src="public/assets/img/site/banner_pneus.png" alt="Banner de Pneus">
+                <img src="public/assets/img/site/banner_utilidades.png" alt="Banner de Pneus">
             </div>
 
         </div>

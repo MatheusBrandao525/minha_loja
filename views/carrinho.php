2398555848090b6eac220e7f4b31d0f870e59663
MatheusBrandao525 <mafe123silva@gmail.com>
2025-01-16 16:40:31 -0400

    feat: :sparkles: Exibindo dados do carrinho do usuario, itens e valores.

--- views/carrinho.php
+++ views/carrinho.php
@@ -1,5 +1,11 @@
 <?php
 require 'components/header.php';
+$carrinhoController = new CarrinhoController();
+$dadosCarrinhoUsuario = $carrinhoController->exibirProdutosCarrinhoUsuario($_SESSION['ID']);
+$subTotalCarrinho = 0;
+$valorTotalCarrinho = 0;
+$frete = 10;
+$desconto = 5;
 ?>
 <div class="row-carrinho">
     <div class="header_carrinho">
@@ -14,40 +20,24 @@ require 'components/header.php';
                 <div class="header_item">V. Unitário</div>
                 <div class="header_item">Total</div>
             </div>
+            <?php foreach ($dadosCarrinhoUsuario as $carrinho) : 
+                $valorTotalProduto = $carrinho['quantidade'] * $carrinho['vlr_unitario'];    
+            ?>
             <div class="carrinho_item">
-                <img src="public/assets/img/placeholder.jpg" alt="Nome do Produto" class="produto_imagem">
-                <span class="produto_nome">Nome do Produto</span>
+                <img src="<?php echo $carrinho['imagem_url'];?>" alt="<?php echo $carrinho['nome_produto'];?>" class="produto_imagem">
+                <span class="produto_nome"><?php echo $carrinho['nome_produto'];?></span>
                 <div class="quantidade_controle">
                     <button class="quantidade_menos">-</button>
-                    <span class="quantidade">1</span>
+                    <span class="quantidade"><?php echo $carrinho['quantidade'];?></span>
                     <button class="quantidade_mais">+</button>
                 </div>
-                <span class="valor_unitario">R$ 00,00</span>
-                <span class="valor_total">R$ 00,00</span>
+                <span class="valor_unitario">R$ <?php echo number_format($carrinho['vlr_unitario'],2,',','.');?></span>
+                <span class="valor_total">R$ <?php echo number_format($valorTotalProduto,2,',','.');?></span>
             </div>
-            <div class="carrinho_item">
-                <img src="public/assets/img/placeholder.jpg" alt="Nome do Produto" class="produto_imagem">
-                <span class="produto_nome">Nome do Produto</span>
-                <div class="quantidade_controle">
-                    <button class="quantidade_menos">-</button>
-                    <span class="quantidade">1</span>
-                    <button class="quantidade_mais">+</button>
-                </div>
-                <span class="valor_unitario">R$ 00,00</span>
-                <span class="valor_total">R$ 00,00</span>
-            </div>
-            <div class="carrinho_item">
-                <img src="public/assets/img/placeholder.jpg" alt="Nome do Produto" class="produto_imagem">
-                <span class="produto_nome">Nome do Produto</span>
-                <div class="quantidade_controle">
-                    <button class="quantidade_menos">-</button>
-                    <span class="quantidade">1</span>
-                    <button class="quantidade_mais">+</button>
-                </div>
-                <span class="valor_unitario">R$ 00,00</span>
-                <span class="valor_total">R$ 00,00</span>
-            </div>
-            <!-- Repita .carrinho_item para cada produto no carrinho -->
+            <?php 
+                $subTotalCarrinho += $carrinho['quantidade'] * $carrinho['vlr_unitario'];
+                endforeach;
+            ?>
         </div>
 
         <div class="info_carrinho">
@@ -55,15 +45,28 @@ require 'components/header.php';
                 <input type="text" placeholder="Código do cupom" id="codigo_cupom">
                 <button id="aplicar_cupom">Usar</button>
             </div>
+            <form action="checkout" method="post">
+            <input type="hidden" value="<?php echo $idUsuarioLogado; ?>" name="idusuario">
+                <input type="hidden" name="totalpedidosemdesconto" value="<?php echo $totalPedido; ?>">
+                <input type="hidden" value="<?php if (!empty($idCarrinho)) {
+                                              echo $idCarrinho;
+                                            } else {
+                                              echo 0;
+                                            } ?>" name="idcarrinho">
+                <input type="hidden" id="valorcomdesconto" value="0" name="valorcomdesconto">
+                <input type="hidden" id="cupomValor" name="cupomValor">
             <div class="detalhes_carrinho">
-                <div class="linha_carrinho"><span>Total Produtos:</span> <span>R$ 00,00</span></div>
-                <div class="linha_carrinho"><span>Desconto:</span> <span>R$ 00,00</span></div>
-                <div class="linha_carrinho"><span>Frete:</span> <span>R$ 00,00</span></div>
-                <div class="linha_carrinho total"><span>Total:</span> <span>R$ 00,00</span></div>
+                <div class="linha_carrinho"><span>Total Produtos:</span> <span>R$ <?php echo number_format($subTotalCarrinho,2,',','.');?></span></div>
+                <div class="linha_carrinho"><span>Desconto:</span> <span>R$ <?php echo number_format($desconto,2,',','.');?></span></div>
+                <div class="linha_carrinho"><span>Frete:</span> <span>R$ <?php echo number_format($frete,2,',','.');?></span></div>
+                <div class="linha_carrinho total"><span>Total:</span> <span>R$ <?php $valorTotalCarrinho = $subTotalCarrinho + $frete - $desconto; echo number_format($valorTotalCarrinho,2,',','.');?></span></div>
             </div>
             <div class="acao_carrinho">
-                <button id="finalizar_pedido">Finalizar Pedido</button>
+            <?php if (!empty($idCarrinho)) { ?>
+                <button type="submit" class="btn btn-block btn-primary" style="display: flex;justify-content:center;">Finalizar Pedido</a>
+                <?php } ?>
             </div>
+            </form>
         </div>
 
     </div>
